501cbeb9cf1fc0bef0881a579bd8f6869d58627a
Durjoy96 <mddurjoy853@gmail.com>
2024-09-18 21:28:42 +0600

    poor login functionality and redirect to the home page has been added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,32 @@
+const ufoImg = document.getElementById("ufo-img");
+
  document.getElementById("loginBtn").addEventListener("click", function(event) {
+   const phone = document.getElementById("phoneNumber").value;
+   const pin = document.getElementById("pinNumber").value;
     event.preventDefault();
-    console.log("okay")
+
+    if(phone === "019" && pin === "1234") {  
+      // window.location.assign("home.html");
+      window.location.href = "home.html";
+    } else {
+      alert("please input a valid phone number or pin");
+    };
+
+ });
+
+ //ufo image animation
+ document.getElementById("phoneNumber").addEventListener("focusin", function() {
+    ufoImg.classList.add("animate-bounce");
+ })
+
+ document.getElementById("phoneNumber").addEventListener("focusout", function() {
+    ufoImg.classList.remove("animate-bounce");
+ });
+
+ document.getElementById("pinNumber").addEventListener("focusin", function() {
+    ufoImg.classList.add("animate-bounce");
+ })
+
+ document.getElementById("pinNumber").addEventListener("focusout", function() {
+    ufoImg.classList.remove("animate-bounce");
  });
